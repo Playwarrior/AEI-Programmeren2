e60f7ea710f8f5d0df31dcc457cfef5fda1ca109
Bryan <khobryan2000@gmail.com>
2019-01-17 19:07:37 +0100

    Removed redundant import lines in the HomePage.
    Created the layout, objects, components and visuals of overlay one.

--- src/com/avans/GUI/HomePage.java
+++ src/com/avans/GUI/HomePage.java
@@ -1,11 +1,7 @@
 package com.avans.GUI;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 /**
  * Home screen of the application
--- src/com/avans/GUI/OverlayOne.java
+++ src/com/avans/GUI/OverlayOne.java
@@ -5,13 +5,78 @@ import java.awt.*;
 
 public class OverlayOne extends JPanel {
 
-    private JLabel title;
-    private JComboBox JCSeries;
+    private JPanel eastPanel;
+    private JComboBox<String> JCSeries;
     private JTable table;
 
     public OverlayOne() {
         this.setLayout(new BorderLayout());
         this.setBackground(Color.DARK_GRAY);
+        addComponents();
+    }
+
+    private void addComponents() {
+        // Overlay title
+        JLabel title = new JLabel("Overzicht 1: Gemiddelde kijktijd van series", SwingConstants.CENTER);
+        title.setFont(new Font("Helvetica Neue", Font.PLAIN, 20));
+        title.setForeground(Color.white);
+
+        // Initializes the objects
+        setEastPanel();
+        setTable();
+
+        this.add(title, BorderLayout.NORTH);
+        this.add(this.eastPanel, BorderLayout.EAST);
+        this.add(this.table, BorderLayout.CENTER);
+        this.add(new JScrollPane(this.table));
+    }
+
+    // Initializes the east panel
+    private void setEastPanel() {
+        this.eastPanel = new JPanel();
+        this.eastPanel.setLayout(new GridLayout(12, 1));
+        this.eastPanel.setBackground(Color.gray);
+        addComponentsToEastPanel();
+    }
+
+    private void addComponentsToEastPanel() {
+        // Description for the JComboBox
+        JLabel chooseSeries = new JLabel("Kies Serie", SwingConstants.CENTER);
+        chooseSeries.setForeground(Color.white);
+        chooseSeries.setFont(new Font("Helvetica Neue", Font.BOLD, 12));
+
+        // Initializes the JComboBox
+        setJCSeries();
+
+        this.eastPanel.add(chooseSeries);
+        this.eastPanel.add(this.JCSeries);
+    }
+
+    private void setTable() {
+        // need to add getEpisodes of Series via Action listener
+        String[] columnNames = {"Serie", "Aflevering no.", "Titel"};
+        String[][] data = {
+                {"Vikings", "1", "Begin"},
+                {"House of Cards", "2", "Eeee"}
+        };
+
+        this.table = new JTable(data, columnNames);
+        this.table.setBounds(30, 40, 200, 300);
+        this.table.setSize(500, 500);
+
+        this.table.setFont(new Font("Helvetica Neue", Font.PLAIN, 12));
+        this.table.setForeground(Color.BLACK);
+    }
+
+    private void setJCSeries() {
+        // add for-loop to add all series of the database
+        String[] seriesNames = new String[]{"Vikings", "House of Cards", "Games of Thrones"};
+
+        this.JCSeries = new JComboBox<>(seriesNames);
+        this.JCSeries.setPreferredSize(new Dimension(130, 10));
+        this.JCSeries.setFont(new Font("Helvetica Neue", Font.PLAIN, 10));
+        this.JCSeries.setBackground(Color.white);
+        this.JCSeries.setForeground(Color.darkGray);
     }
 
 }
